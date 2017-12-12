097f1d4d3e6977fe3e1b7a2138897a04ab5d8d34
Michello Lo <michelle.erica.lo@gmail.com>
2017-12-12 16:32:38 +1100

    resolving conflict

--- priyanka_narasimhan/week01/friday_homework/js/plan_my_trip.js
+++ priyanka_narasimhan/week01/friday_homework/js/plan_my_trip.js
@@ -124,25 +124,6 @@ line6 : ['Grand Central',
 
 
 <<<<<<< HEAD
-// singleLineTrip('lineN', '34th', 'lineN', '8th');
-// singleLineTrip('lineN', '8th', 'lineN', '34th');
-
-console.log(`\n\n\n\n\n`);
-
-const multiLineTrip = function(startLine, startPoint, endLine, endPoint){
-///FIRST TRIP
-    //Change end line to Union Square
-    var endPointInFirstJourney = 'Union Square';
-    var endLineInFirstJourney  = startLine;
-    singleLineTrip(startLine, startPoint, endLineInFirstJourney, endPointInFirstJourney);
-////SECOND TRIP
-    var  startLineInSecondJourney    = endLine;
-    var  startPointInFirstJourney    = 'Union Square';
-    console.log(`Changing lines`);
-    singleLineTrip(startLineInSecondJourney, startPointInFirstJourney, endLine, endPoint);
-};
-=======
->>>>>>> 316e57137ded01d59707e54bf3d2282491087c49
 //
 // <<<<<<< HEAD
 // // singleLineTrip('lineN', '34th', 'lineN', '8th');
