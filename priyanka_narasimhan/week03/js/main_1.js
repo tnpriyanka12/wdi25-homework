549abb0acf3617abf190d5cee015a1ee68ed3e79
tnpriyanka12 <tnpriyanka12@gmail.com>
2017-12-15 11:10:58 +1100

    CAT HOMEWORK

---
+++ priyanka_narasimhan/week03/js/main_1.js
@@ -0,0 +1,105 @@
+// let timerId1 = setInterval(catDance, 100);
+// document.addEventListener('click', function(){
+// setTimeout(clearInterval(timerId), 800);
+// });
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function stopColor() {
+//     clearInterval(myVar);
+// }
+// function setBgImage() {
+//     var x = document.body;
+//     x.style.display = "block";
+//
+//     x.style.backgroundImage = "url('images/rainingcats.jpeg')";
+//
+//     document.addEventListener('click', function(){setBgImage()});
+//     x.style.backgroundImage = "url('images/retro1.jpeg')";
+//     setTimeout(function(){console.log(`waiting`)}, 4000);
+//     document.addEventListener('click', function(){
+//       x.style.backgroundImage = "url('images/retro1.jpeg')";
+// });
+//
+//     //x.style.backgroundImage = "url('images/retro1.jpeg')";
+//     //x.style.backgroundImage = "url('images/download.jpeg')";
+//
+// }
+//
+// function stopBgImage() {
+//     clearInterval(myVar);
+// }
+// var myVar = setInterval(function(){ setColor() }, 300);
+// setTimeout(stopColor, 8000);
+//
+// var myVar = setInterval(function() {catWalk()}, 300);
+// setTimeout(stopWalk, 8000);
+//
+// // var myVar = setInterval(function() {setBgImage()}, 300);
+// // setTimeout(stopWalk, 8000);
+//
+// document.addEventListener('click', function(){setBgImage()});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // //Cat walks left-right in the screen
+// // timerId = setInterval(catWalk, 50);
+// // document.addEventListener('click', function(){
+// //   setTimeout(clearInterval(timerId), 200);
+// // });
+// // document.addEventListener('click', function(){
+// //   timerId = setInterval(catWalk, 200);
+// // });
+// //
+// // document.addEventListener('click', function(){
+// //   let DOM_a;
+// //   DOM_a = document.createElement("img");
+// //   DOM_a.src="images/thinner.jpeg";
+// //   document.getElementById('img').appendChild(DOM_a);
+// //   catimg.src="images/thin_cat.jpeg";
+// // });
+// // // timerId = setInterval(catWalk, 50);
+// // // document.addEventListener('click', function(){
+// // //   setTimeout(clearInterval(timerId), 200);
+// // // });
+// //
+// // document.addEventListener('click', function(){
+// //    let DOM_a;
+// //    DOM_a = document.createElement("img");
+// //    DOM_a.src="images/scared_img.png";
+// //    document.getElementById('img').appendChild(DOM_a);
+// //    catimg.src="images/scared_cat.png";
+// //  });
+// //
+// //
+// // // timerId = setInterval(catReverse, 50);
+// // // timerId = setInterval(catReverse, 50);
+// // // document.addEventListener('click', function(){
+// // //   clearInterval(timerId, 800);
+// // // });
