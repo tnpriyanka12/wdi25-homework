1b4acc4e555a762516faf9c44338797a9441dd42
Grant Hanrahan <grantjhanrahan@gmail.com>
2018-01-15 08:30:37 +1100

    All week 4 homework

---
+++ grant_hanrahan/week04/catsvsdoggos/public/js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    $("#jungle").get(0).play();
+
+
+
+$('h2').funText(25, 'candy');
+$('h1').funText(55, 'reverseRainbow');
+$('a').funText(15, 'reverseCandy');
+
+
+
+});
